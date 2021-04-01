b1cf4904423e66bcff46936c886c488e45b93dcb
Christopher Lin <christopher.lin045@gmail.com>
2021-04-01 13:33:58 -0400

    Remove duplicate scripts, prepbrains now working

--- prepbrains.sh
+++ prepbrains.sh
@@ -3,7 +3,7 @@
 orig=$1
 parentdir=$(dirname "$1")
 filename=$(basename "$(basename "$orig" .nii.gz)" .nii)
-workdir=$parentdir/$filename_working
+workdir=$parentdir/${filename}_working
 mkdir $workdir
 cp $orig $workdir
 # prep environment
@@ -11,4 +11,5 @@ source scripts/prep/prep_env.sh
 # run prep script
 sh scripts/prep/prep_T1w.sh $workdir/${filename}.nii*
 # apply brain mask
-fslmaths $workdir/${filename}.nii.gz -mas *_brain_mask.nii.gz $workdir/${filename}_skullstripped.nii.gz
+fslmaths $workdir/${filename}.nii.gz -mas $workdir/*_brain_mask.nii.gz $workdir/${filename}_skullstripped.nii.gz
+
